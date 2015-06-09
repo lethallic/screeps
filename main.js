b52394d0aab53600912203005d1239f9fcae2cb8
lethallic <lethallic@gmail.com>
2015-06-09 11:58:20 +0200

    Test

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+/// <reference path="typings/screeps/screeps.d.ts" />
 var _ = require("lodash");
+
+var controller = (function(factory) {
+	return factory({
+		
+	});
+})(function(options){
+	
+});
