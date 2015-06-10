9fd1ba67694da0c597716fde8466432d8648937a
lethallic <lethallic@gmail.com>
2015-06-10 09:41:36 +0200

    RoomController

---
+++ config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+	
+	creeps : {
+		"harvesters" : {
+			max : 4,
+			body : [MOVE, CARRY, WORK]	
+		},
+		"builders" : {
+			max : 2,
+			body : [MOVE, CARRY, WORK]
+		}
+	}
+	
+	
+};
--- controller.js
+++ controller.js
@@ -1,3 +1,11 @@
+var _ = require("lodash");
+var config = require("config");
+
+var _roles = {
+	BUILDER : "builder",
+	HARVESTER : "worker"
+};
+
 module.exports = function(rooms){
 	recycleMemory();
 	
@@ -12,17 +20,45 @@ module.exports = function(rooms){
 function RoomController(room) {
 	var self = this;
 	
-	
+	this.factory = new RoomFactory(room, this);
+			
 	this.getHarvesters = function() {
-		
+		return _.filter(room.creeps, {"memory" : {"role" :  _roles.HARVESTER}}); 		
 	};
 	
 	this.getBuilders = function() {
-		
-	}	
-		
+		return _.filter(room.creeps, {"memory" : {"role" :  _roles.HARVESTER}});
+	};	
 }
 
+function RoomFactory(room, controller) {
+	
+	var getIdleSpawn = function() {
+		
+		return null;
+	}
+	
+	var createCreep = function(body, role) {
+		
+		return false;
+	}
+	
+	
+	this.createHarvester = function() {
+		var cfg = config.creeps.harvesters;
+		if ( controller.getHarvester().length < cfg.max ) {
+			createCreep(cfg.body, _roles.HARVESTER);
+		}		
+	}
+	
+	this.createBuilder = function() {
+		var cfg = config.creeps.builders;
+		if ( controller.getHarvester().length < cfg.max ) {
+			createCreep(cfg.body, _roles.BUILDER);
+		}
+	};	
+			
+}
 
 function recycleMemory() {
 	for (var i in Memory.creeps) {
