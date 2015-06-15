cf18d371bf350bacf1ebc1bb19a63b3a590fd553
Lethallic <lethallic@gmail.com>
2015-06-15 17:21:10 +0000

    controller fixes

--- extend.js
+++ extend.js
@@ -1,12 +1,49 @@
-module.exports = function() {
-    Creep.prototype.setStatus = function(s) {
-    this.memory.status = s ;
-    };
+function extend(p, ext) {
+    for ( var e in ext ) {
+        p[e] = ext[e];
+    }
+};
 
-    Creep.prototype.getStatus = function(def) {
-        if ( this.memory && this.memory.status ) {
-            return this.memory.status;    
+
+module.exports = function() {
+    
+    extend(Creep.prototype, {
+        getStatus : function(default) {
+            if ( this.memory && this.memory.status ) {
+                return this.memory.status;    
+            }
+            return def || null;
+        },
+        setStatus : function(newStatus) {
+            this.memory.status = newStatus;
         }
-        return def || null;
-    };
-}
+    });
+    
+    extend(Room.prototype, {
+        
+    });
+    
+    extend(Structure.prototype, {
+        isDamaged : function() {
+            return this.hits < this.maxHits;
+        }
+    });
+    
+    
+    extend(Source.prototype, {
+        isDefended : function() {
+            var targets = this.pos.findInRange(FIND_HOSTILE_CREEPS, 10);
+            if ( targes.length ) {
+                return true;
+            }
+            
+            targets =  this.pos.findInRange(FIND_HOSTILE_STRUCTURES, 10);
+            if ( targets.length ) {
+                return true;
+            }
+
+            return false;
+        }
+    });
+
+};
